faa6590e5ce2fe51d683a9e99f9c4e6f50cf1472
Gavin Grant <gavingrant@gmail.com>
2022-02-02 20:06:06 -0800

    feat: add view project cta button

--- components/About.js
+++ components/About.js
@@ -4,17 +4,24 @@ import profile from '../public/headshot.jpg'
 
 const About = () => {
   return (
-    <div className="container mx-auto px-4 py-24 mt-0 lg:mt-6 max-w-5xl">
-      <div className="item-center flex flex-col-reverse text-center lg:mx-4 lg:flex-row lg:text-left lg:items-center">
+    <div className="container mx-auto mt-0 max-w-5xl px-4 py-16 sm:py-20 md:py-24 lg:mt-6">
+      <div className="item-center flex flex-col-reverse text-center lg:mx-4 lg:flex-row lg:items-center lg:text-left">
         <div className="lg:px-4">
           <h1 className="text-2xl font-bold text-gray-900 dark:text-white lg:text-5xl">
             Hi there, I'm Gavin.
           </h1>
           <div className="mt-6 text-gray-800 dark:text-white">
             <p className="mb-4">
-              I'm a developer leveraging his architecture background to utilize JavaScript, TypeScript, React, Next.js, Node.js, Express, and other modern web development frameworks.
+              I'm a developer leveraging his architecture background to utilize
+              JavaScript, TypeScript, React, Next.js, Node.js, Express, and
+              other modern web development frameworks.
             </p>
           </div>
+          <button className="btn-primary mt-2">
+            <a href="/projects">
+              View Projects
+            </a>
+          </button>
         </div>
         <div className="mb-10 flex-shrink-0 lg:mt-12 lg:px-4">
           <Image
