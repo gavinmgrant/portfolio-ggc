3465a53c46b1a781d38d2cb39a603f0d3555aa12
Gavin Grant <gavingrant@gmail.com>
2022-05-15 14:14:59 -0700

    refactor: about copy

--- components/About.js
+++ components/About.js
@@ -4,7 +4,7 @@ import profile from '../public/headshot.png'
 
 const About = () => {
   return (
-    <div className="container mx-auto my-20 max-w-xl lg:max-w-5xl px-4 h-full lg:my-0 lg:h-screen flex justify-center items-center">
+    <div className="container mx-auto my-20 flex h-full max-w-xl items-center justify-center px-4 lg:my-0 lg:h-screen lg:max-w-5xl">
       <div className="item-center flex flex-col-reverse text-center lg:mx-4 lg:flex-row lg:items-center lg:text-left">
         <div className="lg:px-4">
           <h1 className="text-2xl font-semibold text-gray-900 dark:text-white lg:text-5xl">
@@ -12,9 +12,12 @@ const About = () => {
           </h1>
           <div className="mt-6 text-gray-800 dark:text-white">
             <p className="mb-4">
-              I'm a developer leveraging his architecture background to utilize
-              JavaScript, TypeScript, React, Next.js, Node.js, Express, and
-              other modern web development frameworks.
+              I'm a software engineer at{' '}
+              <a href="https://sessionslive.com/" target="_blank">
+                Sessions
+              </a>
+              , a live video streaming service that connects musicians to a
+              global audience.
             </p>
           </div>
           <button className="btn-primary mt-2">
