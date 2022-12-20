360e4cd2ab7b59e7a452050cefad930638bb2dd5
Gavin Grant <gavingrant@gmail.com>
2022-12-20 11:47:44 -0800

    chore: update next, add loader for project images

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -12,13 +12,13 @@ export default function Projects() {
 
   if (error)
     return (
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex h-screen items-center justify-center">
         <p>Failed to load projects.</p>
       </div>
     )
   if (!data)
     return (
-      <div className="flex items-center justify-center h-screen">
+      <div className="flex h-screen items-center justify-center">
         <Loader />
       </div>
     )
@@ -33,7 +33,7 @@ export default function Projects() {
         <link rel="icon" href="/icon.png" />
       </Head>
 
-      <main className="container mx-auto pt-20 lg:pt-24 grid grid-cols-1 gap-x-6 gap-y-14 px-4 lg:max-w-6xl lg:grid-cols-2">
+      <main className="container mx-auto grid grid-cols-1 gap-x-6 gap-y-14 px-4 pt-20 lg:max-w-6xl lg:grid-cols-2 lg:pt-24">
         {data.map((project) => {
           return (
             <Link
@@ -43,19 +43,29 @@ export default function Projects() {
               <div key={project.slug}>
                 <div
                   className="overflow-hidden rounded-md shadow-lg"
-                  style={{ position: 'relative', maxWidth: '600px', maxHeight: '400px' }}
+                  style={{
+                    position: 'relative',
+                    maxWidth: '600px',
+                    maxHeight: '400px',
+                  }}
                 >
-                  <Image
-                    alt={project.title}
-                    src={project.imgsrc}
-                    width={600}
-                    height={400}
-                    layout="responsive"
-                    className="cursor-pointer overflow-hidden rounded-md transition-all duration-300 ease-in-out hover:scale-105"
-                  />
+                  {!project.imgsrc ? (
+                    <Loader />
+                  ) : (
+                    <Image
+                      alt={project.title}
+                      src={project.imgsrc}
+                      width={600}
+                      height={400}
+                      layout="responsive"
+                      className="cursor-pointer overflow-hidden rounded-md transition-all duration-300 ease-in-out hover:scale-105"
+                    />
+                  )}
                 </div>
                 <div className="cursor-pointer">
-                  <h2 className="my-4 text-2xl font-semibold">{project.name}</h2>
+                  <h2 className="my-4 text-2xl font-semibold">
+                    {project.name}
+                  </h2>
                   <p>{project.description}</p>
                 </div>
               </div>
