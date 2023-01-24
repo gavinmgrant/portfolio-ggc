e7c326a726c8457fb6586bf91510ab711265b840
Gavin Grant <gavingrant@gmail.com>
2023-01-24 12:48:18 -0800

    fix: landing page margin width

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -4,14 +4,14 @@ import { wrap } from 'popmotion'
 
 const testimonials = [
   {
-    name: 'Alex Kambeitz',
-    title: 'Software Engineer at Sessions',
-    quote: `"Working with Gavin was one of the highlights of my time at Sessions. His dedication to building solid UI components and his attention to detail made him stand out as a developer on my team, and I knew I could rely on him to tackle whatever task I sent his way. He brought with him the talent and experience needed to make his mark on the engineering team in the best way. Gavin is also a delightful person just to get to know! Even in a full remote world, Gavin was kind and thoughtful in every interaction I had with him. The one time I finally got to meet him in person, it felt like seeing a friend I'd known for years. Any team would be beyond fortunate to work alongside him."`,
-  },
-  {
     name: 'Cyd La Luz',
     title: 'Software Engineer at Google',
     quote: `"Gavin Grant is a highly adaptable professional who, as my student in Bloc, demonstrated not only a strong grasp of core coding concepts but also regularly pushed himself to do research and work beyond the curriculum to ensure that he had not only a well built profile, but also to ensure that he built genuinely useful applications. Gavin is a builder in the best sense: he cares about the best standards in technology, elegance and economy in code and design, and he gets work done. I strongly encourage anyone looking at Gavin's profile to check out the work he's done as it really speaks for itself."`,
+  },
+  {
+    name: 'Alex Kambeitz',
+    title: 'Software Engineer at Sessions',
+    quote: `"Working with Gavin was one of the highlights of my time at Sessions. His dedication to building solid UI components and his attention to detail made him stand out as a developer on my team, and I knew I could rely on him to tackle whatever task I sent his way. He brought with him the talent and experience needed to make his mark on the engineering team in the best way. Gavin is also a delightful person just to get to know! Even in a full remote world, Gavin was kind and thoughtful in every interaction I had with him. The one time I finally got to meet him in person, it felt like seeing a friend I'd known for years. Any team would be beyond fortunate to work alongside him."`,
   },
   {
     name: 'Spencer Skeen',
@@ -51,7 +51,10 @@ const Testimonials = () => {
   }
 
   return (
-    <div className="relative w-screen text-center lg:ml-4 lg:text-left" style={{ height: "65vh" }}>
+    <div
+      className="relative w-full text-center lg:ml-4 lg:text-left"
+      style={{ height: '65vh' }}
+    >
       <motion.h2
         className="mb-8 text-3xl font-semibold text-gray-900 dark:text-white lg:mb-12 lg:text-4xl"
         initial={{ opacity: 0, y: 20 }}
