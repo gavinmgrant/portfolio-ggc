288cda5b54d2db24450d68bdd312ebc46438dcd8
Gavin Grant <gavingrant@gmail.com>
2023-02-22 17:29:18 -0800

    refactor: about animation

--- components/About.js
+++ components/About.js
@@ -12,7 +12,7 @@ const About = () => {
             className="text-4xl font-semibold text-gray-900 dark:text-white lg:text-5xl"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 4 }}
+            transition={{ delay: 4.25 }}
           >
             Hi there, I'm Gavin.
           </motion.h1>
@@ -20,7 +20,7 @@ const About = () => {
             className="mt-2 lg:mt-6 text-gray-800 dark:text-white"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 4.25 }}
+            transition={{ delay: 4.5 }}
           >
             <p className="mb-4">
               I'm a software engineer leveraging my architecture background to
@@ -31,7 +31,7 @@ const About = () => {
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 4.5 }}
+            transition={{ delay: 4.75 }}
           >
             <motion.button
               className="btn-primary lg:mt-2"
@@ -44,9 +44,9 @@ const About = () => {
         </div>
         <motion.div
           className="mb-6 lg:mb-10 flex-shrink-0 lg:mt-12 lg:pl-4"
-          initial={{ opacity: 0, x: 60 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ delay: 4.75 }}
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ delay: 4 }}
         >
           <Image
             src={profile}
