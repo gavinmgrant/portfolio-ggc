4282b1a8889652245e9da42f3eb219e44023cecb
Gavin Grant <gavingrant@gmail.com>
2023-03-02 18:52:00 -0800

    fix: center hero image

--- components/About.js
+++ components/About.js
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion'
 const About = () => {
   return (
     <div className="container mx-auto my-20 flex h-full max-w-xl items-center justify-center px-4 lg:my-0 lg:h-screen lg:max-w-5xl">
-      <div className="item-center flex flex-col-reverse text-center lg:mx-4 lg:flex-row lg:items-center lg:text-left">
+      <div className="flex flex-col-reverse items-center justify-center text-center lg:mx-4 lg:flex-row lg:text-left">
         <div className="lg:px-4">
           <motion.h1
             className="text-4xl font-semibold text-gray-900 dark:text-white lg:text-5xl"
