35a1f9a28fd16dc0f45c4afca6699f6604a899ca
Gavin Grant <gavingrant@gmail.com>
2023-03-04 16:00:10 -0800

    refactor: footer icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,45 +1,97 @@
 import React from 'react'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
+import {
+  IconBrandTwitter,
+  IconBrandLinkedin,
+  IconBrandGithub,
+  IconMail,
+} from '@tabler/icons'
 
 const Footer = () => {
   const year = new Date().getFullYear()
 
   return (
-    <div className="mx-auto py-12 mt-0">
-      <div className="mx-auto px-4 text-gray-800 dark:text-white lg:max-w-6xl">
+    <div className="mx-auto mt-0 py-12">
+      <div className="mx-auto px-4 text-sm text-gray-800 dark:text-white lg:max-w-6xl">
         <div className="dark:border-white-300 mb-4 border-t-2 border-gray-300"></div>
         <div className="flex flex-col items-center justify-between lg:flex-row">
-          <div className="flex flex-wrap space-x-2 pt-2 sm:space-x-4 lg:pt-0">
-            <a
+          <div className="flex flex-col space-y-2 space-x-0 pt-2 sm:flex-row sm:space-y-0 sm:space-x-4 sm:pt-0">
+            <motion.a
               href="https://twitter.com/higavingrant"
-              className="transition-colors duration-500 hover:text-yellow-600 dark:hover:text-yellow-500"
+              className="flex items-center space-x-1 transition-colors duration-500 hover:text-yellow-600 dark:hover:text-yellow-500"
               target="_blank"
               rel="noreferrer"
+              whileHover={{ scale: 1.05 }}
             >
-              Twitter
-            </a>
-            <a
+              <IconBrandTwitter />
+              <div className="overflow-hidden text-clip">
+                <motion.div
+                  initial={{ x: -30, opacity: 0 }}
+                  whileInView={{ x: 0, opacity: 1 }}
+                  transition={{ delay: 0, type: 'spring', bounce: 0.1 }}
+                  viewport={{ once: true }}
+                >
+                  Twitter
+                </motion.div>
+              </div>
+            </motion.a>
+            <motion.a
               href="https://www.linkedin.com/in/gavinmgrant/"
-              className="transition-colors duration-500 hover:text-yellow-600 dark:hover:text-yellow-500"
+              className="flex items-center space-x-1 transition-colors duration-500 hover:text-yellow-600 dark:hover:text-yellow-500"
               target="_blank"
               rel="noreferrer"
+              whileHover={{ scale: 1.05 }}
             >
-              LinkedIn
-            </a>
-            <a
+              <IconBrandLinkedin />
+              <div className="overflow-hidden text-clip">
+                <motion.div
+                  initial={{ x: -30, opacity: 0 }}
+                  whileInView={{ x: 0, opacity: 1 }}
+                  transition={{ delay: 0.2, type: 'spring', bounce: 0.1 }}
+                  viewport={{ once: true }}
+                >
+                  LinkedIn
+                </motion.div>
+              </div>
+            </motion.a>
+            <motion.a
               href="https://github.com/gavinmgrant"
-              className="transition-colors duration-500 hover:text-yellow-600 dark:hover:text-yellow-500"
+              className="flex items-center space-x-1 transition-colors duration-500 hover:text-yellow-600 dark:hover:text-yellow-500"
               target="_blank"
               rel="noreferrer"
+              whileHover={{ scale: 1.05 }}
             >
-              GitHub
-            </a>
-            <a
+              <IconBrandGithub />
+              <div className="overflow-hidden text-clip">
+                <motion.div
+                  initial={{ x: -30, opacity: 0 }}
+                  whileInView={{ x: 0, opacity: 1 }}
+                  transition={{ delay: 0.4, type: 'spring', bounce: 0.1 }}
+                  viewport={{ once: true }}
+                >
+                  GitHub
+                </motion.div>
+              </div>
+            </motion.a>
+
+            <motion.a
               href="mailto:gavin@gavingrant.co?subject=Hi Gavin!"
-              className="transition-colors duration-500 hover:text-yellow-600 dark:hover:text-yellow-500"
+              className="flex items-center space-x-1 transition-colors duration-500 hover:text-yellow-600 dark:hover:text-yellow-500"
+              whileHover={{ scale: 1.05 }}
             >
-              Email
-            </a>
+              <IconMail />
+              <div className="overflow-hidden text-clip">
+                <motion.div
+                  initial={{ x: -30, opacity: 0 }}
+                  whileInView={{ x: 0, opacity: 1 }}
+                  transition={{ delay: 0.6, type: 'spring', bounce: 0.1 }}
+                  viewport={{ once: true }}
+                >
+                  Email
+                </motion.div>
+              </div>
+            </motion.a>
           </div>
           <div className="mt-6 flex flex-col items-center text-sm lg:mt-0 lg:flex-row">
             <Link
