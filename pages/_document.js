efbe8218f802985a9cffb3edb2555d3bd5ca45b8
Gavin Grant <gavingrant@gmail.com>
2025-01-20 21:19:43 -0800

    feat: google analytics tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -7,11 +8,30 @@ class MyDocument extends Document {
   }
 
   render() {
+    const measurementId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
+
     return (
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&family=DM+Serif+Text:wght@300;400&display=swap" rel="stylesheet" />  
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&family=DM+Serif+Text:wght@300;400&display=swap"
+            rel="stylesheet"
+          />
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+          />
+          <Script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', ${measurementId});
+            `,
+            }}
+          />
         </Head>
         <body>
           <Main />
