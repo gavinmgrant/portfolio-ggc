0879133c08bef14305a7e4486f37a8dfc6d186d4
Gavin Grant <gavingrant@gmail.com>
2025-01-20 22:46:58 -0800

    feat: add mobile menu

--- components/Navigation.js
+++ components/Navigation.js
@@ -3,12 +3,15 @@ import { useEffect, useState } from 'react'
 import ThemeSwitch from './ThemeSwitch'
 import LogoHeader from './LogoHeader'
 import { useTheme } from 'next-themes'
+import { IconMenu2 } from '@tabler/icons'
+import { Drawer, DrawerContent, DrawerBody, useDisclosure } from '@heroui/react'
 
 const Navigation = () => {
   const { theme } = useTheme()
   const [underlineClass, setUnderlineClass] = useState(
     'link-underline link-underline-light'
   )
+  const { isOpen, onOpen, onOpenChange } = useDisclosure()
 
   useEffect(() => {
     if (theme === 'dark') {
@@ -20,22 +23,58 @@ const Navigation = () => {
 
   return (
     <div className="dark:border-white-300 fixed top-0 z-20 mx-auto mb-12 w-screen border-b-[0.5px] border-neutral-300 bg-white dark:bg-neutral-900">
-      <div className="mx-auto flex items-center justify-between px-4 py-3 lg:max-w-6xl lg:py-6">
+      <div className="mx-auto flex items-center justify-between px-4 py-4 sm:max-w-6xl sm:py-6">
         <Link
           href="/"
           className="transition-colors duration-500 hover:text-yellow-600 dark:hover:text-yellow-500"
         >
           <LogoHeader />
         </Link>
-        <div className="flex items-center gap-4 lg:gap-5">
-          <Link
-            href="/projects"
-            className="text-sm transition-colors duration-500 hover:text-yellow-600 dark:hover:text-yellow-500 lg:text-base"
-          >
-            <span className={underlineClass}>Projects</span>
-          </Link>
-          <ThemeSwitch />
+
+        {/* Mobile Menu */}
+        <IconMenu2
+          className="block cursor-pointer sm:hidden"
+          onClick={onOpen}
+        />
+        <Drawer
+          placement="bottom"
+          backdrop="blur"
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+        >
+          <DrawerContent>
+            {(onClose) => (
+              <DrawerBody className="px-8 py-12">
+                <div className="flex flex-col items-center justify-center gap-6">
+                  <Link
+                    href="/projects"
+                    className="transition-colors duration-500 hover:text-yellow-600 dark:hover:text-yellow-500"
+                  >
+                    <span className={underlineClass} onClickCapture={onClose}>
+                      Projects
+                    </span>
+                  </Link>
+                  <ThemeSwitch />
+                </div>
+              </DrawerBody>
+            )}
+          </DrawerContent>
+        </Drawer>
+        {/* Mobile Menu */}
+
+        {/* Desktop Menu */}
+        <div className="hidden sm:block">
+          <div className="flex flex-row items-center justify-center gap-5">
+            <Link
+              href="/projects"
+              className="transition-colors duration-500 hover:text-yellow-600 dark:hover:text-yellow-500"
+            >
+              <span className={underlineClass}>Projects</span>
+            </Link>
+            <ThemeSwitch />
+          </div>
         </div>
+        {/* Desktop Menu */}
       </div>
     </div>
   )
