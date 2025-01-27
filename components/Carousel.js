97722ec0cfe171af93a955e1047dafd1523ee96c
Gavin Grant <gavin@happycompanies.com>
2025-01-27 13:47:32 -0800

    refactor: preload images in carousel

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,16 +1,14 @@
 import { motion, AnimatePresence } from 'framer-motion'
 import { useState, useEffect } from 'react'
-import Image from 'next/image'
+import NextImage from 'next/image'
 import Loader from './Loader'
 import { getSanityImageUrl } from '../utils/getSanityImageUrl'
 import { IconArrowNarrowLeft, IconArrowNarrowRight } from '@tabler/icons'
 
 const Carousel = ({ sanityImages, projectName }) => {
   const [current, setCurrent] = useState(0)
-  const [isLoaded, setIsLoaded] = useState(false)
-  const [isInitialLoaded, setIsInitialLoaded] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
   const [images, setImages] = useState([])
-  const [isHovered, setIsHovered] = useState(false)
 
   if (!Array.isArray(sanityImages) || sanityImages.length === 0) {
     return (
@@ -21,22 +19,48 @@ const Carousel = ({ sanityImages, projectName }) => {
   }
 
   useEffect(() => {
-    setIsInitialLoaded(false)
-    let imageUrls = sanityImages.map((image) =>
+    const imageUrls = sanityImages.map((image) =>
       getSanityImageUrl(image.asset._ref)
     )
     setImages(imageUrls)
-    setTimeout(() => setIsLoaded(true), 3000)
+
+    const preloadImages = imageUrls.map(
+      (src) =>
+        new Promise((resolve) => {
+          const img = new Image()
+          img.src = src
+          img.onload = resolve
+          img.onerror = resolve
+        })
+    )
+
+    Promise.all(preloadImages).then(() => {
+      setIsLoading(false)
+    })
   }, [sanityImages])
 
+  const preloadImage = (src, callback) => {
+    const img = new Image()
+    img.src = src
+    img.onload = callback
+  }
+
+  const changeSlide = (index) => {
+    setIsLoading(true)
+    preloadImage(images[index], () => {
+      setCurrent(index)
+      setIsLoading(false)
+    })
+  }
+
   const nextSlide = () => {
-    setIsLoaded(false)
-    setCurrent((prev) => (prev === images.length - 1 ? 0 : prev + 1))
+    const nextIndex = (current + 1) % images.length
+    changeSlide(nextIndex)
   }
 
   const prevSlide = () => {
-    setIsLoaded(false)
-    setCurrent((prev) => (prev === 0 ? images.length - 1 : prev - 1))
+    const prevIndex = (current - 1 + images.length) % images.length
+    changeSlide(prevIndex)
   }
 
   const variants = {
@@ -50,94 +74,75 @@ const Carousel = ({ sanityImages, projectName }) => {
     visible: { opacity: 1, transition: { duration: 0.3 } },
   }
 
-  const handleInitialLoad = () => {
-    setIsInitialLoaded(true)
-    setIsLoaded(true)
-  }
-
-  const handleLoaded = () => {
-    setIsLoaded(true)
-  }
-
   return (
-    <div
-      className="relative aspect-[2860/1614] w-full rounded-lg object-contain shadow-lg shadow-neutral-300 dark:shadow-neutral-700 lg:w-[940px]"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <div className="relative h-full w-full">
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={current}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            variants={variants}
-            className="h-full w-full overflow-hidden"
-          >
-            <Image
-              alt={projectName}
-              src={images[current]}
-              width={940}
-              height={531}
-              quality={100}
-              className={`h-full w-full overflow-hidden rounded-lg object-cover ${
-                !isLoaded && 'animate-pulse'
-              }`}
-              onLoadingComplete={
-                current === 0 ? handleInitialLoad : handleLoaded
-              }
-              priority={true}
-            />
-          </motion.div>
-        </AnimatePresence>
+    <div className="relative aspect-[2860/1614] w-full rounded-lg object-contain shadow-lg shadow-neutral-300 dark:shadow-neutral-700 lg:w-[940px]">
+      <div className="absolute inset-0 flex items-center justify-center rounded-lg bg-neutral-300 dark:bg-neutral-700">
+        <Loader />
       </div>
 
-      {(!isInitialLoaded || !isLoaded) && (
-        <div className="absolute left-0 top-0 z-10 flex aspect-[2860/1614] w-full animate-pulse items-center justify-center rounded-lg bg-neutral-300 dark:bg-neutral-700 lg:h-[531px] lg:w-[940px]">
-          <Loader className="z-20" />
+      {!isLoading && (
+        <div className="relative z-10 h-full w-full">
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={current}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              variants={variants}
+              className="h-full w-full overflow-hidden"
+            >
+              <NextImage
+                alt={projectName}
+                src={images[current]}
+                width={940}
+                height={531}
+                quality={100}
+                className={`h-full w-full rounded-lg object-cover ${
+                  isLoading && 'opacity-0'
+                }`}
+                onLoadingComplete={() => setIsLoading(false)}
+                priority={current === 0}
+              />
+            </motion.div>
+          </AnimatePresence>
         </div>
       )}
 
       {/* Left arrow */}
-      <AnimatePresence>
-        {isHovered && sanityImages.length > 1 && (
-          <motion.div
-            initial="hidden"
-            animate="visible"
-            exit="hidden"
-            variants={arrowVariants}
-            className="absolute top-1/2 left-4 z-20 -translate-y-1/2 transform"
+      {sanityImages.length > 1 && (
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          variants={arrowVariants}
+          className="absolute left-4 top-1/2 z-20 -translate-y-1/2 transform"
+        >
+          <button
+            onClick={prevSlide}
+            className="rounded-full bg-neutral-900 bg-opacity-50 p-2 text-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-opacity-70 focus:outline-none active:scale-75"
           >
-            <button
-              onClick={prevSlide}
-              className="rounded-full bg-neutral-900 bg-opacity-50 p-2 text-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-opacity-70 focus:outline-none active:scale-75"
-            >
-              <IconArrowNarrowLeft />
-            </button>
-          </motion.div>
-        )}
-      </AnimatePresence>
+            <IconArrowNarrowLeft />
+          </button>
+        </motion.div>
+      )}
 
       {/* Right arrow */}
-      <AnimatePresence>
-        {isHovered && sanityImages.length > 1 && (
-          <motion.div
-            initial="hidden"
-            animate="visible"
-            exit="hidden"
-            variants={arrowVariants}
-            className="absolute top-1/2 right-4 z-20 -translate-y-1/2 transform"
+      {sanityImages.length > 1 && (
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+          variants={arrowVariants}
+          className="absolute right-4 top-1/2 z-20 -translate-y-1/2 transform"
+        >
+          <button
+            onClick={nextSlide}
+            className="rounded-full bg-neutral-900 bg-opacity-50 p-2 text-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-opacity-70 focus:outline-none active:scale-75"
           >
-            <button
-              onClick={nextSlide}
-              className="rounded-full bg-neutral-900 bg-opacity-50 p-2 text-white transition-all duration-300 ease-in-out hover:scale-105 hover:bg-opacity-70 focus:outline-none active:scale-75"
-            >
-              <IconArrowNarrowRight />
-            </button>
-          </motion.div>
-        )}
-      </AnimatePresence>
+            <IconArrowNarrowRight />
+          </button>
+        </motion.div>
+      )}
 
       {/* Pagination */}
       {sanityImages.length > 1 && (
@@ -145,7 +150,7 @@ const Carousel = ({ sanityImages, projectName }) => {
           {images.map((_, idx) => (
             <button
               key={idx}
-              onClick={() => setCurrent(idx)}
+              onClick={() => changeSlide(idx)}
               className={`h-3 w-3 rounded-full ${
                 current === idx ? 'bg-black dark:bg-white' : 'bg-neutral-400'
               }`}
