054faef470111cfc208d651368d20a4f2075064f
Gavin Grant <gavingrant@gmail.com>
2025-10-26 14:13:40 -0700

    refactor(hero): copy update add logo animation

--- components/About.js
+++ components/About.js
@@ -1,13 +1,82 @@
-import Image from 'next/image'
+import { useState } from 'react'
 import { useRouter } from 'next/router'
-import headshotDark from '../public/images/headshot-dark.jpg'
 import { motion } from 'motion/react'
 import { IconBrandLinkedin, IconBrandGithub } from '@tabler/icons-react'
 import { Button } from '@heroui/react'
 import { urls } from '../configs/urls.config'
+import { delay } from 'motion'
 
 const About = () => {
   const router = useRouter()
+  const [isHovered, setIsHovered] = useState(false)
+
+  const logoSpring = {
+    type: 'spring',
+    stiffness: 300,
+    damping: 50,
+    ease: 'easeInOut',
+    duration: 1,
+  }
+
+  const LogoLeft = () => (
+    <motion.div
+      className="w-[86.4px] text-black dark:text-white"
+      animate={isHovered ? { x: -8, y: 8 } : { x: 8, y: 0 }}
+      transition={logoSpring}
+    >
+      <svg viewBox="0 0 216 342" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M194.205 121L21 21V221L194.205 321V221L96.7772 164.75"
+          stroke="currentColor"
+          stroke-width="42"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </motion.div>
+  )
+  const LogoRight = () => (
+    <motion.div
+      className="w-[86.4px] text-black dark:text-white"
+      animate={isHovered ? { x: 8, y: 8 } : { x: -8, y: 0 }}
+      transition={logoSpring}
+    >
+      <svg viewBox="0 0 216 342" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M194.205 21.0035L21 121.004V321.004L194.205 221.004"
+          stroke="currentColor"
+          stroke-width="42"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </motion.div>
+  )
+  const LogoTop = () => (
+    <motion.div
+      className="absolute -top-12 w-[155.6px] text-black dark:text-white"
+      animate={isHovered ? { y: -16 } : { y: 6 }}
+      transition={logoSpring}
+    >
+      <svg viewBox="0 0 389 242" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M367.41 121L194.205 21L21 121L194.205 221L280.808 171L183.38 114.75"
+          stroke="currentColor"
+          stroke-width="42"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </motion.div>
+  )
+
+  const handleHover = (hovered) => {
+    if (hovered) {
+      setIsHovered(hovered)
+    } else {
+      setTimeout(() => setIsHovered(hovered), 300)
+    }
+  }
 
   return (
     <>
@@ -18,20 +87,14 @@ const About = () => {
           className="flex max-w-[965px] flex-col-reverse items-center justify-center gap-4 text-center sm:gap-6 md:gap-8 lg:flex-row lg:gap-20 lg:text-left"
         >
           <div>
-            <h1 className="heading-size-lg font-semibold text-neutral-900 dark:text-white">
-              Building fast, scalable websites and web apps
+            <h1 className="heading-size-lg font-semibold text-neutral-900 dark:text-white leading-snug">
+              Building web architecture that performs.
             </h1>
             <div className="mt-2 text-neutral-800 dark:text-white lg:mt-4">
               <p className="ftext-sm lg:text-base">
-                I specialize in creating high-performance, scalable websites and
-                web applications that deliver exceptional digital experiences.
-                With expertise in modern frontend technologies like Next.js,
-                Vue.js, TypeScript, and Sanity, I build fast, dynamic solutions
-                tailored to your business needs. Whether you're looking for a
-                sleek marketing site, a powerful web application, or anything in
-                between, I focus on delivering innovative, user-friendly
-                solutions that not only meet your goals but also ensure
-                seamless, engaging experiences for your users.
+                I build fast, scalable web experiences with React, Next.js, Vue,
+                and TypeScript - engineered for performance and tailored to your
+                goals.
               </p>
             </div>
             <div className="mt-4 flex w-full flex-col items-center justify-center gap-5 sm:h-12 sm:flex-row lg:mt-6 lg:justify-start">
@@ -109,16 +172,18 @@ const About = () => {
               </div>
             </div>
           </div>
-          <div className="mb-0 w-[148px] flex-shrink-0 sm:w-[200px] md:w-[224px] lg:mb-10 lg:mt-12 lg:w-[264px] lg:pl-2">
-            <Image
-              src={headshotDark}
-              alt="Headshot of Gavin Grant"
-              priority={true}
-              className="rounded-full"
-              width={260}
-              height={260}
-              placeholder="blur"
-            />
+          <div
+            className="mb-0 flex w-[148px] items-center justify-center sm:w-[200px] md:w-[224px] lg:mb-10 lg:mt-12 lg:w-[264px] lg:pl-2"
+            onMouseEnter={() => handleHover(true)}
+            onMouseLeave={() => handleHover(false)}
+          >
+            <div className="relative mt-16 flex flex-col items-center justify-center pb-8 sm:pb-4 lg:mt-0 lg:pb-0">
+              <LogoTop className="" />
+              <div className="itemce-center flex">
+                <LogoLeft />
+                <LogoRight />
+              </div>
+            </div>
           </div>
         </motion.div>
       </div>
